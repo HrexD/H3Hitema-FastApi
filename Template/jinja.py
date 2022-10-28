edd7be335294a0217f542acb379ab8e4cef39cbc
HrexD <cedgoujon78@gmail.com>
2022-10-28 14:09:42 +0200

    ajout du 28/10

--- Template/jinja.py
+++ Template/jinja.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 templates = Jinja2Templates(directory="templates")
@@ -11,4 +13,4 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/game/{id}", response_class=HTMLResponse)
 async def read(requests: Request, game: str, id: int):
-    return templates.TemplateResponse("index.html", {"request": requests, "Jeu": game, "id": id})
+    return templates.TemplateResponse("index.html", {"request": requests, "game": game, "id": id})
